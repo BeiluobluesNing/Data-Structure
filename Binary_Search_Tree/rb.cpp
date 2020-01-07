60d5efd02cb329e3e05ec318ebd7782de1c41f47
BeiluobluesNing <529599524@qq.com>
2020-01-07 22:03:56 +0800

    rbtree insertfixup

--- Binary_Search_Tree/rb.cpp
+++ Binary_Search_Tree/rb.cpp
@@ -88,6 +88,62 @@ void rbtree<T>::insert(T key)
 	rbinsetFixup(tmp);
 }
 
+template<class T>
+void rbtree<T>::rbinsetFixup(_rbtreeNode<T> *p){
+	//红红情况 祖父肯定是黑 并且需要考虑上面的情况
+	while(p->parent->color == true)
+	{
+		if(p->parent == p->parent->parent->left)
+		{
+			_rbtreeNode<T> *uncle = p->parent->parent->right;
+			//叔叔也为红 变色处理
+			if(uncle->color == true)
+			{
+				p->parent->color =false;
+				uncle->color = false;
+				p->parent->parent->color = true;
+				p = p->parent->parent;
+				//根的情况最后处理
+			}
+			//叔的为黑，旋转处理
+			else{
+				//z字型 旋转
+				if(p = p->parent->right)
+				{
+					p = p->parent;
+					leftRotate(p);
+				}
+				p->parent->color = false;
+				p->parent->parent->color = true;
+				p->parent->right->color = false;
+				rightRotate(p->parent->parent)
+			}
+		}
+		else{
+			_rbtreeNode<T> *uncle = p->parent->parent->left;
+			if(uncle->color == true)
+			{
+				p->parent->color = false;
+				uncle->color = false;
+				p->parent->parent->color = true;
+				p = p->parent->parent;
+			}
+			else{
+				if(p->parent->left)
+				{
+					p = p->parent;
+					rightRotate(p);
+				}
+				p->parent->color = false;
+				p->parent->parent->color = true;
+				p->parent->left->color = false;
+				leftRotate(p->parent->parent);
+			}
+		}
+	}
+	root->color = false;
+}
+
 template <class T>
 _rbtreeNode<T> *rbtree<T>::search(T key)
 {
@@ -114,7 +170,7 @@ _rbtreeNode<T> * rbtree<T>::treeSuccessor(_rbtreeNode<T> *p)
 {
 	if(p->right != nil) 
 	{
-		p = p->right;	//原代码没有这个
+		p = p->right;	//
 		while(p->left != nil)
 			p = p->left;
 		return p;
